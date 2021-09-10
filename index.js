2ca7347bb12d0508b3e8006635aa49a7a7b7cf34
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-11 01:39:22 +0500

    Globally applying middleware

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send(`
@@ -16,7 +18,7 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.post('/', bodyParser.urlencoded({ extended: true }), (req, res) => {
+app.post('/', (req, res) => {
     console.log(req.body);
     res.send('Account created');
 });
