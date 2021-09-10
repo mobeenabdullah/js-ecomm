0622829b0a8132eaf253397b15925a54e1dc1c49
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-10 16:53:40 +0500

    Creating a web server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hi there');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hi there from the server');
+});
+
+app.listen(3000, () => {
+    console.log('Listening...');
+});
