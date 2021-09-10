53b956dccf88a564bccd68fa7110aeb52a10446d
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-11 02:01:29 +0500

    Opening the Repo data

--- repositories/users.js
+++ repositories/users.js
@@ -13,6 +13,24 @@ class UsersRepository {
             fs.writeFileSync(this.filename, '[]');
         }
     }
+
+    async getAll() {
+        // Open the file called this.filename
+        const contents = await fs.promises.readFile(this.filename, { encoding: 'utf8' });
+
+        // Read its contents
+        console.log(contents);
+
+        // Parse the contents
+
+        // Return the parsed data
+    }
 }
 
-const repo = new UsersRepository('users1.json');
+const test = async () => {
+    const repo = new UsersRepository('users.json');
+
+    await repo.getAll();
+}
+
+test();
