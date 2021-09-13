3171bf5e0d35c6f2c3b2b5a6f98b2ec4da64ed09
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-13 07:12:47 +0500

    Comparing hashed passwords

--- repositories/users.js
+++ repositories/users.js
@@ -44,6 +44,13 @@ class UsersRepository {
         return record;
     }
 
+    async comparePasswords(saved, supplied) {
+        const [hashed, salt] = saved.split('.');
+        const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+
+        return hashed === hashedSuppliedBuf.toString('hex');
+    }
+
     async writeAll(records) {
         await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
     }
