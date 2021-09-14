d5e21a0d0f924f273c882c01f8ac3f67da1eba16
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-14 13:48:55 +0500

    Remaining sign in validation + Template Helper Functions

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,20 +1,15 @@
 const layout = require('../layout');
+const { getError } = require('../../helpers');
 
-const getError = (errors, prop) => {
-    try {
-        return errors.mapped()[prop].msg;
-    } catch(err) {
-        return '';
-    }
-};
-
-module.exports = () => {
+module.exports = ({ errors }) => {
     return layout({
         content: `
             <div>
                 <form method="POST">
                     <input name="email" placeholder="email" />
+                    ${getError(errors, 'email')}
                     <input name="password" placeholder="password" />
+                    ${getError(errors, 'password')}
                     <button>Sign In</button>
                 </form>
             </div>
