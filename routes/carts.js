116762a40090f33ae42c45b904c08aa536f194da
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-17 05:20:05 +0500

    Shopping Cart Boilerplate

---
+++ routes/carts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Receive a POST request to add an item to a cart
+
+// Receive a GET request to show all items in the cart
+
+// Receive a POST request to delete an item to a cart
+
+module.exports = router;
