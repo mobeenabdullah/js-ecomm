3c5ee2c7d628a5eeb8974e53a7f38907c78f2234
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-17 15:21:43 +0500

    Added Qty in rendered list

--- views/carts/show.js
+++ views/carts/show.js
@@ -3,7 +3,7 @@ const layout = require('../layout');
 module.exports = ({ items }) => {
     const renderedItems = items.map(item => {
         return `
-            <div>${item.product.title} - ${item.product.price}</div>
+            <div>${item.product.title} - ${item.product.price} - ${item.quantity}</div>
         `;
     }).join('');
 
